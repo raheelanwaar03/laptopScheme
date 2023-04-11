bc7a31344d9d03f0a4a9b468bc1acbb43f544c44
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-04-11 14:10:06 +0500

    okay now

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->nullable();
             $table->string('education');
             $table->string('laptop');
             $table->string('mobile');
             $table->string('age');
+            $table->string('password')->nullable();
             $table->string('role')->default('user');
             $table->rememberToken();
             $table->timestamps();
